288420b8c7149f792bb7eb6fbc0751c1c544d34f
Jack Howard <jackhowaa@gmail.com>
2020-07-06 10:42:31 -0500

    Update gatsby-browser.js
    Use update if shown with service worker https://www.gatsbyjs.org/docs/add-offline-support-with-a-service-worker/#displaying-a-message-when-a-service-worker-updates

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,14 @@
 // prism seems to use custom typefaces monsterrat, but overrode
 import "prismjs/themes/prism.css"
 import "./src/styles/global.css"
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  )
+
+  if (answer === true) {
+    window.location.reload()
+  }
+}
